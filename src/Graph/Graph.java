f0dfaff56b1b497d06b5d5d27e6f18858a98be08
DevTamakuwala <dev.official2424@gmail.com>
2025-01-07 23:02:43 +0530

    Graph
    Signed-off-by: DevTamakuwala <dev.official2424@gmail.com>

---
+++ src/Graph/Graph.java
@@ -0,0 +1,20 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+    private final HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+    public boolean addVertex(String vertex) {
+        if (adjList.get(vertex) == null) {
+            adjList.put(vertex, new ArrayList<>());
+            return true;
+        }
+        return false;
+    }
+}
--- src/HashTable/Main.java
+++ src/HashTable/Main.java
@@ -1,4 +1,4 @@
-import HashTable.HashTable;
+package HashTable;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
