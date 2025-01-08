3488bb7807b399d4624fbcd92284e37a804bd707
DevTamakuwala <dev.official2424@gmail.com>
2025-01-08 18:06:58 +0530

    Graph complete
    Signed-off-by: DevTamakuwala <dev.official2424@gmail.com>

--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -5,6 +5,32 @@ public class Main {
         Graph graph = new Graph();
 
         graph.addVertex("A");
-        graph.printGraph();
+        graph.addVertex("B");
+        graph.addVertex("C");
+//        graph.addVertex("D");
+
+        if (graph.addEdge("A", "B") && graph.addEdge("A", "C")) {
+            graph.printGraph();
+        } else {
+            System.out.println("There is some issue with the Vertexes, Please check your graph.");
+            graph.printGraph();
+        }
+
+        if (graph.removeVertex("D")) {
+            System.out.println("Vertex deleted..!!");
+            graph.printGraph();
+        } else {
+            System.out.println("Vertex not available.");
+            graph.printGraph();
+        }
+
+//        if(graph.removeEdge("A","B")){
+//            System.out.println("Edge deleted successfully..!");
+//            graph.printGraph();
+//        } else {
+//            System.out.println("There is some issue with the edges, Please the graph.");
+//            graph.printGraph();
+//        }
+
     }
 }
