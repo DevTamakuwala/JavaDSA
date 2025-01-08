bccdedc7a2074088ad41b0701a2c441f048f90ab
DevTamakuwala <dev.official2424@gmail.com>
2025-01-08 22:46:31 +0530

    Heap complete
    Signed-off-by: DevTamakuwala <dev.official2424@gmail.com>

---
+++ src/Heap/Main.java
@@ -0,0 +1,22 @@
+package Heap;
+
+public class Main {
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        heap.insert(95);
+        heap.insert(75);
+        heap.insert(80);
+        heap.insert(55);
+        heap.insert(60);
+        heap.insert(50);
+        heap.insert(65);
+        System.out.println(heap.getHeap());
+        heap.insert(100);
+        System.out.println(heap.getHeap());
+
+        System.out.println(heap.remove() + " Removed");
+        System.out.println(heap.getHeap());
+        System.out.println(heap.remove() + " Removed");
+        System.out.println(heap.getHeap());
+    }
+}
