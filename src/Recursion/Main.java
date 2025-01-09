c5b0dc16edf6afb5c81ce5818345efc731a0267e
DevTamakuwala <dev.official2424@gmail.com>
2025-01-09 22:52:25 +0530

    Recursion complete
    Signed-off-by: DevTamakuwala <dev.official2424@gmail.com>

---
+++ src/Recursion/Main.java
@@ -0,0 +1,9 @@
+package Recursion;
+
+public class Main {
+    public static void main(String[] args) {
+        Recursion recursion = new Recursion();
+
+        System.out.println(recursion.factorial(8));
+    }
+}
---
+++ src/Recursion/Recursion.java
@@ -0,0 +1,9 @@
+package Recursion;
+
+public class Recursion {
+
+    public int factorial(int n) {
+        if (n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+}
