52121e1c7eaf652bb2bea5b3aa2808ea3853046f
DevTamakuwala <dev.official2424@gmail.com>
2025-01-11 13:03:24 +0530

    Selection sort complete
    Signed-off-by: DevTamakuwala <dev.official2424@gmail.com>

---
+++ src/Algorithms/sorts/selectionsort/SelectionSort.java
@@ -0,0 +1,31 @@
+package Algorithms.sorts.selectionsort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 6, 5, 1, 3};
+
+        System.out.println(Arrays.toString(arr));
+
+        selectionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
