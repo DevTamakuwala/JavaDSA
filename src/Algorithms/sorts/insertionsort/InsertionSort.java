a96525a67177da9055f076ea9facfe693540ac24
DevTamakuwala <dev.official2424@gmail.com>
2025-01-11 13:03:09 +0530

    Insertion sort complete
    Signed-off-by: DevTamakuwala <dev.official2424@gmail.com>

---
+++ src/Algorithms/sorts/insertionsort/InsertionSort.java
@@ -0,0 +1,25 @@
+package Algorithms.sorts.insertionsort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 6, 5, 1, 3};
+
+        insertionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j > -1 && temp < array[j]) {
+                array[j + 1] = array[j];
+                array[j] = temp;
+                j--;
+            }
+        }
+    }
+}
