ae5a1d44ec5682621e5989f9e019790955153bf8
DevTamakuwala <dev.official2424@gmail.com>
2025-01-12 19:18:28 +0530

    Fibonacci with Bottom Up approach complete
    Signed-off-by: DevTamakuwala <dev.official2424@gmail.com>

--- src/Algorithms/fibonacci/Fib_No_Memo.java
+++ src/Algorithms/fibonacci/Fib_No_Memo.java
@@ -9,11 +9,12 @@ public class Fib_No_Memo {
     public static void main(String[] args) {
         int n = 400;
         System.out.println("\nFib of " + n + " = " + fib(n));
+        System.out.println("\nFib of " + n + " = " + fib_no_memo(n));
         System.out.println("\nCounter: " + counter);
         System.out.println("\nMemo: " + Arrays.toString(memo));
     }
 
-    public static int fib(int n) {
+    public static int fib_no_memo(int n) {
         counter++;
         if (memo[n] != null) {
             return memo[n];
@@ -22,7 +23,19 @@ public class Fib_No_Memo {
             memo[n] = n;
             return n;
         }
-        memo[n] = fib(n - 1) + fib(n - 2);
+        memo[n] = fib_no_memo(n - 1) + fib(n - 2);
         return memo[n];
+    }
+
+    public static int fib(int n) {
+        int[] fibList = new int[n + 1];
+        fibList[0] = 0;
+        fibList[1] = 1;
+
+        for (int index = 2; index <= n; index++) {
+            counter++;
+            fibList[index] = fibList[index - 1] + fibList[index - 2];
+        }
+        return fibList[n];
     }
 }
