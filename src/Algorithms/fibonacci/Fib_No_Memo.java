7993c555e13c6e90f42f0dfa97177e4e462754f1
DevTamakuwala <dev.official2424@gmail.com>
2025-01-12 18:12:55 +0530

    Fibonacci with Memoization complete
    Signed-off-by: DevTamakuwala <dev.official2424@gmail.com>

---
+++ src/Algorithms/fibonacci/Fib_No_Memo.java
@@ -0,0 +1,28 @@
+package Algorithms.fibonacci;
+
+import java.util.Arrays;
+
+public class Fib_No_Memo {
+    static Integer[] memo = new Integer[1000];
+    static int counter = 0;
+
+    public static void main(String[] args) {
+        int n = 400;
+        System.out.println("\nFib of " + n + " = " + fib(n));
+        System.out.println("\nCounter: " + counter);
+        System.out.println("\nMemo: " + Arrays.toString(memo));
+    }
+
+    public static int fib(int n) {
+        counter++;
+        if (memo[n] != null) {
+            return memo[n];
+        }
+        if (n == 0 || n == 1) {
+            memo[n] = n;
+            return n;
+        }
+        memo[n] = fib(n - 1) + fib(n - 2);
+        return memo[n];
+    }
+}
